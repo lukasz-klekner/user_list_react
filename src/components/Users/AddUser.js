3aaf8c90b8551d97dc8c69de1e54d5881993b414
Łukasz Klekner <lukaszklekner@gmail.com>
2022-02-23 22:04:11 +0100

    implements uncontrolled inputs

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,25 +1,21 @@
 import styled from 'styled-components'
-import { useState, memo } from 'react'
+import { useState, useRef, memo } from 'react'
 
 import Button from '../UI/Button'
 import Card from '../UI/Card'
 import ErrorModal from '../UI/ErrorModal'
 
 const AddUser = ({ className, onAddUser }) => {
-  const [name, setName] = useState('')
-  const [age, setAge] = useState('')
   const [error, setError] = useState('')
 
-  const usernameChangeHandler = (event) => {
-    setName(event.target.value)
-  }
-
-  const ageChangeHandler = (event) => {
-    setAge(event.target.value)
-  }
+  const nameRef = useRef()
+  const ageRef = useRef()
 
   const addUserHandler = (event) => {
     event.preventDefault()
+
+    const name = nameRef.current.value
+    const age = ageRef.current.value
 
     if (name.trim().length === 0 || age.trim().length === 0) {
       setError({
@@ -38,8 +34,9 @@ const AddUser = ({ className, onAddUser }) => {
     }
 
     onAddUser(name, age)
-    setName('')
-    setAge('')
+
+    nameRef.current.value = ''
+    ageRef.current.value = ''
   }
 
   const errorHandler = () => {
@@ -58,19 +55,9 @@ const AddUser = ({ className, onAddUser }) => {
       <Card>
         <form className={className} onSubmit={addUserHandler}>
           <label htmlFor='username'>Username</label>
-          <input
-            id='username'
-            type='text'
-            value={name}
-            onChange={usernameChangeHandler}
-          />
+          <input id='username' type='text' ref={nameRef} />
           <label htmlFor='age'>Age (Years)</label>
-          <input
-            id='age'
-            type='number'
-            value={age}
-            onChange={ageChangeHandler}
-          />
+          <input id='age' type='number' ref={ageRef} />
           <Button type='submit'>Add User</Button>
         </form>
       </Card>
